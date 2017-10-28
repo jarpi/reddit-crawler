934454cfd7858d09db9db11dfb86eb985b53b8a3
Josep <josep.arpi@gmail.com>
2017-10-28 12:28:05 +0200

    Refactor data extractor, add tests

---
+++ index.js
@@ -0,0 +1,30 @@
+const getDataByPath = ( data, path, fields ) => {
+    return resolveObj(data, path)
+    .reduce(uniqueElements, [{}])
+    .reduce(convertToArray, null)
+}
+
+const resolveObj = ( obj, path ) => {
+    const key = path[0]
+    if(!key) return obj
+    if (Array.isArray(obj)) return resolveArray(obj, path, key)
+    return resolveObj(obj[key], path.slice(1,path.length))
+}
+
+const resolveArray  = ( obj, path, key ) => {
+    return obj.map( item => {
+        return resolveObj(item[key], path.slice(1, path.length))
+    })
+}
+
+const uniqueElements = ( obj, element ) => {
+    if (!obj[0][element]) obj[0][element] = true
+    return obj
+}
+
+const convertToArray = ( _, obj) => {
+    return Object.keys(obj)
+}
+
+module.exports = getDataByPath
+
---
+++ spec/unit/parser.spec.js
@@ -0,0 +1,20 @@
+const _ = require('lodash')
+const mocha = require('mocha')
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const expect = chai.expect
+const parser = require('../../index.js')
+
+describe('Data extractor', function(){
+    const dataSet = require('../fixtures/programming-dataset.json')
+    const mapByDomain = ( element ) => element.data.domain
+
+    it('Should return chosen fields from dataset', function(done){
+        const expected =_.uniq(_.flatMap(dataSet['data']['children'], mapByDomain))
+        const actual = parser(dataSet, ['data', 'children', 'data', 'domain'])
+        expect(actual.length).to.equal(expected.length)
+        expect(actual).to.have.members(expected)
+        done()
+    })
+})
