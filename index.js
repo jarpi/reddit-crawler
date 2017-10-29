46b9b8c6bf6d2c1c2a780578f6b6c716a5ba99df
Josep <josep.arpi@gmail.com>
2017-10-29 19:10:42 +0100

    Feature/Bugfix MVP - Add pagination - Process gathered data - Fix queryParams reducer

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const redditClient = require('./lib/redditSdk.js')
+const parser = require('./lib/dataParser.js')
 
-const getProgPosts = () => {
-    redditClient.readToken()
+const getProgPosts = (after) => {
+    return redditClient.getToken()
         .then(() => {
-            const queryParams = {'limit': 1}
+            const queryParams = {'limit': 100}
+            if (after) queryParams.after = after
             return redditClient.getProgrammingRedditPosts(queryParams)
-        })
-        .then( res => {
-            console.dir(res)
-            return
         })
         .catch( err => {
             if (err.error === 401) {
@@ -21,4 +19,18 @@ const getProgPosts = () => {
 
 }
 
-getProgPosts()
+const getDataUpToDate = (after,acc) => {
+  const data = acc || []
+  const targetDate  = Date.parse('September 21, 2017')
+  getProgPosts(after)
+  .then( res => {
+    const dataLength = res.data.children.length
+    const lastDate = res.data.children[dataLength-1].data.created*1000
+    console.dir(new Date(lastDate))
+    data.push(parser(res, ['data', 'children', 'data', 'domain']))
+    console.dir(data)
+    if (lastDate>targetDate && res.data.after !== null) getDataUpToDate(res.data.after, data)
+  })
+}
+
+getDataUpToDate();
