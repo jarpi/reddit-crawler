9e03d352ab042e8b9a92887ebfa6c1e4b2029780
Josep <josep.arpi@gmail.com>
2017-10-29 17:08:03 +0100

    Refactor

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-const getDataByPath = ( data, path, fields ) => {
-    return resolveObj(data, path)
-    .reduce(uniqueElements, [{}])
-    .reduce(convertToArray, null)
+const redditClient = require('./lib/redditSdk.js')
+
+const getProgPosts = () => {
+    redditClient.readToken()
+        .then(() => {
+            const queryParams = {'limit': 1}
+            return redditClient.getProgrammingRedditPosts(queryParams)
+        })
+        .then( res => {
+            console.dir(res)
+            return
+        })
+        .catch( err => {
+            if (err.error === 401) {
+                return redditClient.rebuildToken()
+                    .then(getProgPosts)
+            }
+            console.log('error')
+            console.dir(err)
+        })
+
 }
 
-const resolveObj = ( obj, path ) => {
-    const key = path[0]
-    if(!key) return obj
-    if (Array.isArray(obj)) return resolveArray(obj, path, key)
-    return resolveObj(obj[key], path.slice(1,path.length))
-}
-
-const resolveArray  = ( obj, path, key ) => {
-    return obj.map( item => {
-        return resolveObj(item[key], path.slice(1, path.length))
-    })
-}
-
-const uniqueElements = ( obj, element ) => {
-    if (!obj[0][element]) obj[0][element] = true
-    return obj
-}
-
-const convertToArray = ( _, obj) => {
-    return Object.keys(obj)
-}
-
-module.exports = getDataByPath
-
+getProgPosts()
