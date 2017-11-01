1486cddc2da511bf7cc91d744b4e5012b8f6e584
Josep <josep.arpi@gmail.com>
2017-11-01 11:10:45 +0100

    Add tests for dataParser

--- index.js
+++ index.js
@@ -2,36 +2,36 @@ const redditClient = require('./lib/redditSdk.js')
 const parser = require('./lib/dataParser.js')
 
 const getProgPosts = (after) => {
-    return redditClient.getToken()
-        .then(() => {
-            const queryParams = {'limit': 100}
-            if (after) queryParams.after = after
-            return redditClient.getProgrammingRedditPosts(queryParams)
-        })
-        .catch( err => {
-            if (err.error === 401) {
-                return redditClient.rebuildToken()
-                    .then(getProgPosts)
-            }
-            console.log('error')
-            console.dir(err)
-        })
+	return redditClient.getToken()
+		.then(() => {
+			const queryParams = {'limit': 100}
+			if (after) queryParams.after = after
+			return redditClient.getProgrammingRedditPosts(queryParams)
+		})
+		.catch( err => {
+			if (err.error === 401) {
+				return redditClient.rebuildToken()
+					.then(getProgPosts)
+			}
+			console.log('error')
+			console.dir(err)
+		})
 
 }
 
 const getDataUpToDate = (after,acc) => {
-    let data = acc || []
-    const targetDate  = Date.parse('September 21, 2017')
-    return getProgPosts(after)
-        .then( res => {
-            const dataLength = res.data.children.length
-            const lastDate = res.data.children[dataLength-1].data.created*1000
-            // console.dir(new Date(lastDate))
-            data = parser(res, ['data', 'children', 'data', 'domain'], data)
-            if (lastDate>targetDate && res.data.after !== null) return getDataUpToDate(res.data.after, data)
-            return data
-        })
+	let data = acc || []
+	const targetDate  = Date.parse('September 21, 2017')
+	return getProgPosts(after)
+		.then( res => {
+			const dataLength = res.data.children.length
+			const lastDate = res.data.children[dataLength-1].data.created*1000
+			// console.dir(new Date(lastDate))
+			data = parser(res, ['data', 'children', 'data', 'domain'], data)
+			if (lastDate>targetDate && res.data.after !== null) return getDataUpToDate(res.data.after, data)
+			return data
+		})
 }
 
 getDataUpToDate()
-    .then(v=>{console.dir(v)})
+	.then(v=>{console.dir(v)})
--- spec/unit/parser.spec.js
+++ spec/unit/parser.spec.js
@@ -1,8 +1,6 @@
 const _ = require('lodash')
 const mocha = require('mocha')
 const chai = require('chai')
-const chaiAsPromised = require('chai-as-promised')
-chai.use(chaiAsPromised)
 const expect = chai.expect
 const parser = require('../../lib/dataParser.js')
 
@@ -10,11 +8,28 @@ describe('Data extractor', function(){
     const dataSet = require('../fixtures/programming-dataset.json')
     const mapByDomain = ( element ) => element.data.domain
 
-    it('Should return chosen fields from dataset', function(done){
+    it('Should return unique values chosen from dataset as an object', function(done){
         const expected =_.uniq(_.flatMap(dataSet['data']['children'], mapByDomain))
+        console.time('dataParser')
         const actual = parser(dataSet, ['data', 'children', 'data', 'domain'], [])
+        console.timeEnd('dataParser')
+        expect(actual).to.be.an('Array');
         expect(actual.length).to.equal(expected.length)
         expect(actual).to.have.members(expected)
         done()
     })
+
+    it('Should return unique values chosen from dataset as an array', function(done){
+        const expected =_.uniq(_.flatMap(dataSet['data']['children'], mapByDomain))
+        console.time('dataParser')
+        const actual = parser(dataSet['data']['children'], ['data', 'domain'], [])
+        console.timeEnd('dataParser')
+        expect(actual).to.be.an('Array');
+        expect(actual.length).to.equal(expected.length)
+        expect(actual).to.have.members(expected)
+        done()
+    })
+
+    it.skip('Should return error if path is not found', function(done){})
 })
+
