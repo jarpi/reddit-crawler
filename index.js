1cdcc231e58779d0ee9a77b886518256e173d8de
Josep <josep.arpi@gmail.com>
2017-11-25 19:45:09 +0100

    Major refactor

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-const redditClient = require('./lib/redditSdk.js')
-const parser = require('./lib/dataParser.js')
+const redditClient = require('./lib/RedditSdk.js')
+const parser = require('./lib/DataParser.js')
 
 const getProgPosts = (after) => {
-	return redditClient.getToken()
-		.then(() => {
-			const queryParams = {'limit': 100}
-			if (after) queryParams.after = after
-			return redditClient.getProgrammingRedditPosts(queryParams)
-		})
+		const queryParams = {'limit': 100}
+		if (after) queryParams.after = after
+		return redditClient.getProgrammingRedditPosts(queryParams)
 		.catch( err => {
-			if (err.error === 401) {
-				return redditClient.rebuildToken()
-					.then(getProgPosts)
-			}
 			console.log('error')
 			console.dir(err)
 		})
 
 }
 
-const getDataUpToDate = (after,acc) => {
+const buildUniqueDomainsArrByDate = (after,acc) => {
 	let data = acc || []
 	const targetDate  = Date.parse('September 21, 2017')
 	return getProgPosts(after)
@@ -28,10 +21,10 @@ const getDataUpToDate = (after,acc) => {
 			const lastDate = res.data.children[dataLength-1].data.created*1000
 			// console.dir(new Date(lastDate))
 			data = parser(res, ['data', 'children', 'data', 'domain'], data)
-			if (lastDate>targetDate && res.data.after !== null) return getDataUpToDate(res.data.after, data)
+			if (lastDate>targetDate && res.data.after !== null) return buildUniqueDomainsArrByDate(res.data.after, data)
 			return data
 		})
 }
 
-getDataUpToDate()
+buildUniqueDomainsArrByDate()
 	.then(v=>{console.dir(v)})
